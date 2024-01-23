f3b15891681726389b11c559efee5378032fc798
zZzHQTzZz <118146952+zZzHQTzZz@users.noreply.github.com>
2024-01-23 22:26:59 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git modemband https://github.com/4IceG/luci-app-modemband.git >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
